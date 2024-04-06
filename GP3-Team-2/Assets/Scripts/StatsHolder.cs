23e0bbdc39806f4bfa428b00c634a212a216fc13
reireizo <nathan.irwin2021@gmail.com>
2024-04-05 21:42:21 -0400

    4/5/24 Stats and Inventory Script
    Created a script that tracks player stats and inventory (with inventory variables to be added later when we have things to store in it). Also made a static script that is used to save the stats between scenes.

---
+++ GP3-Team-2/Assets/Scripts/StatsHolder.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsHolder
+{
+    public static int savedHealthLevel = 1;
+    public static int savedStamLevel = 1;
+    public static int savedDamageLevel = 1;
+
+    //very simple script. It holds Stat levels between scenes.
+}
