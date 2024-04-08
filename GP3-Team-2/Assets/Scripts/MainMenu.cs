5601da3da55a6f21fd4bb2a256c57d06f03f9194
ChelseaSoto <ch951840@ucf.edu>
2024-04-07 22:51:37 -0400

    added audio

--- GP3-Team-2/Assets/Scripts/MainMenu.cs
+++ GP3-Team-2/Assets/Scripts/MainMenu.cs
@@ -5,35 +5,57 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioClip buttonSound;
+    public AudioSource audioSource;
+
     // This function will make it so if you press the play button, you will be moved to the next scene (game) 
     public void PlayButton()
     {
-        SceneManager.LoadScene("Level 1");
+        StartCoroutine(PlayAndLoad("Level 1"));
     }
 
     public void Controls()
     {
-        SceneManager.LoadScene("Controls");
+        StartCoroutine(PlayAndLoad("Controls"));
     }
 
     public void GoBackToMain()
     {
-        SceneManager.LoadScene("Main Menu");
+        StartCoroutine(PlayAndLoad("Main Menu"));
     }
 
     public void CreditScene()
     {
-        SceneManager.LoadScene("Credits");
+        StartCoroutine(PlayAndLoad("Credits"));
     }
 
     public void Level1()
     {
-        SceneManager.LoadScene("Level 1");
+        StartCoroutine(PlayAndLoad("Level 1"));
     }
 
     // This function will make it so if you press the quit button, the application will close
     public void QuitButton()
     {
+        StartCoroutine(PlayAndQuit());
+    }
+
+    private IEnumerator PlayAndLoad(string Scene){
+        PlayButtonSound();
+        yield return new WaitForSecondsRealtime(buttonSound.length);
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(Scene);
+    }
+
+    private IEnumerator PlayAndQuit(){
+        PlayButtonSound();
+        yield return new WaitForSecondsRealtime(buttonSound.length);
         Application.Quit();
+    }
+
+    private void PlayButtonSound(){
+        if(audioSource != null && buttonSound != null){
+            audioSource.PlayOneShot(buttonSound);
+        }
     }
 }
