b97affe11388e0485c9a1dd3b72180c4a6cf271a
NicolasSpohn <149342232+NicolasSpohn@users.noreply.github.com>
2024-04-21 22:31:01 -0400

    Fresno Boss

---
+++ GP3-Team-2/Assets/Scripts/FresnoBoss.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FresnoBoss : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent agent;
+    public Transform player;
+    GameObject mainCharacter;
+
+    [Header("Health Parameters")]
+    public float health;
+    public float maxHealth = 500f;
+    bool isCapturable;
+
+    Animator animator;
+
+
+    [Header("Attack Parameters")]
+    public float attackRange = 1f;
+    //bool alreadyAttacked = false;
+    //public float timeBetweenAttacks;
+
+    private void Awake()
+    {
+        player = GameObject.Find("player_character_BL_rigged Variant").transform;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isCapturable)
+        {
+            ChasePlayer();
+        }
+        else if (isCapturable) 
+        {
+            animator.SetTrigger("Stun");
+            agent.SetDestination(transform.position);
+        }
+        if(AttackRangeCheck())
+        {
+            Attack();
+        }
+        CheckCapturable();
+        CheckHealth();
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void Attack()
+    {
+        //Debug.Log("Attack");
+        animator.SetTrigger("Attack");
+    }
+
+    private void CheckCapturable()
+    {
+        if (health <= 100)
+        {
+            isCapturable = true;
+        }
+    }
+
+    private void CheckHealth()
+    {
+        if(health > maxHealth)
+        {
+            health = maxHealth;
+        }
+        else if(health < 0)
+        {
+            health = 0;
+        }
+
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    bool AttackRangeCheck()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if(distanceToPlayer <= attackRange)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {   
+        if (other.gameObject.tag == "Bullet")
+        {
+            health -= 50f;
+        }
+        
+        if (isCapturable)
+        {
+            if(other.gameObject.tag == "Net")
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
