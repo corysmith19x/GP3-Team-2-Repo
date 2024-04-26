41fcec051943dfc697bbb6d93f53897e23055adc
ChelseaSoto <ch951840@ucf.edu>
2024-04-25 21:03:52 -0400

    Functionality for menus completed.

--- GP3-Team-2/Assets/Scripts/MainMenu.cs
+++ GP3-Team-2/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public AudioClip buttonSound;
     public AudioSource audioSource;
+    public GameObject loadingScreen;
 
     // This function will make it so if you press the play button, you will be moved to the next scene (game) 
     public void PlayButton()
@@ -38,9 +39,11 @@ public class MainMenu : MonoBehaviour
 
     private IEnumerator PlayAndLoad(string Scene){
         PlayButtonSound();
-        yield return new WaitForSecondsRealtime(buttonSound.length);
+        loadingScreen.SetActive(true);
         Time.timeScale = 1f;
+        yield return new WaitForSecondsRealtime(1.25f);
         SceneManager.LoadSceneAsync(Scene);
+        //loadingScreen.SetActive(false);
     }
 
     private IEnumerator PlayAndQuit(){
