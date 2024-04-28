8a38d89a139f98c367d144520cded62a92a10ff8
corysmith19x <110064828+corysmith19x@users.noreply.github.com>
2024-04-28 17:36:31 -0400

    cutscenes are now skippable with Pause/Esc

--- GP3-Team-2/Assets/Scripts/uiCamTrigger.cs
+++ GP3-Team-2/Assets/Scripts/uiCamTrigger.cs
@@ -24,7 +24,7 @@ public class uiCamTrigger : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape)){
+        if(Input.GetButtonDown("Pause")){
             StartCoroutine(PlayAndLoadScene(loadScene));
         }
 
