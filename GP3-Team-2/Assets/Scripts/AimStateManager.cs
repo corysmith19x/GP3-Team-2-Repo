1e39740089cb5544e8f052ab9349428f87a0f125
corysmith19x <110064828+corysmith19x@users.noreply.github.com>
2024-04-28 18:19:21 -0400

    fixed controller input being slow, edited trigger size of pickups

--- GP3-Team-2/Assets/Scripts/AimStateManager.cs
+++ GP3-Team-2/Assets/Scripts/AimStateManager.cs
@@ -8,6 +8,7 @@ public class AimStateManager : MonoBehaviour
     public Cinemachine.AxisState xAxis, yAxis;
     [SerializeField] Transform camFollowPos;
     [SerializeField] float mouseSense;
+    [SerializeField] float rightStickSensitivity;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +19,11 @@ public class AimStateManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xAxis.Value += Input.GetAxisRaw("Mouse X") * mouseSense;
-        yAxis.Value -= Input.GetAxisRaw("Mouse Y") * mouseSense;
+        xAxis.Value += Input.GetAxisRaw("Right Stick X") * rightStickSensitivity * Time.deltaTime;
+        yAxis.Value -= Input.GetAxisRaw("Right Stick Y") * rightStickSensitivity * Time.deltaTime;
+
+        xAxis.Value += (Input.GetAxisRaw("Mouse X") * mouseSense);
+        yAxis.Value -= (Input.GetAxisRaw("Mouse Y") * mouseSense);
         yAxis.Value = Mathf.Clamp(yAxis.Value, -60, 60);
     }
 
